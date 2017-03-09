4de4f6e3a111dcaadf63d60ec7efe462262579e1
build <andyrthomson@gmail.com>
2017-03-09 20:00:03 +1100

    disable X11

--- include/config.h
+++ include/config.h
@@ -43,7 +43,7 @@
  * Some combinations make no sense.  See the installation document.
  */
 #define TTY_GRAPHICS	/* good old tty based graphics */
-#define X11_GRAPHICS	/* X11 interface */
+/* #define X11_GRAPHICS */	/* X11 interface */
 /* #define QT_GRAPHICS */ 	/* Qt interface */
 /* #define GNOME_GRAPHICS */ 	/* Gnome interface */
 /* #define MSWIN_GRAPHICS */	/* Windows NT, CE, Graphics */
